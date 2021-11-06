3262ed105c9759c11c76376ebcb9cccbf65b3ad2
Alina <alinka_zajceva@list.ru>
2021-11-06 18:38:31 +0300

    the end of the second lesson

--- js/partners.js
+++ js/partners.js
@@ -2,8 +2,15 @@ const renderItems = (data) => {
     console.log(data);
 };
 
-fetch(`./db/partners.json`)
+/* for (let i = 0; 1 < renderItems.length; i++) {
+    console.log(renderItems[i]);
+} */
+
+fetch('./db/partners.json')
     .then((response) => response.json())
     .then((data) => 
     {renderItems(data);
+})
+.catch((error) => {
+    console.log(error);
 });
